6f00c48202d274b11bc715b6e7aac582a9b90ab6
Thangtea <riomiyachi.1991@gmail.com>
2019-01-14 00:43:51 -0800

    Updates

---
+++ precache-manifest.458d8913e7b2ced7ca1fa85c51cd8661.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "869e2a8a696ef80ffe98",
+    "url": "/Thangzaliana-Thangtea/crud-react-redux/static/js/runtime~main.869e2a8a.js"
+  },
+  {
+    "revision": "385fb1c32892c1bd9253",
+    "url": "/Thangzaliana-Thangtea/crud-react-redux/static/js/main.385fb1c3.chunk.js"
+  },
+  {
+    "revision": "e619f176a531ffbad94a",
+    "url": "/Thangzaliana-Thangtea/crud-react-redux/static/js/1.e619f176.chunk.js"
+  },
+  {
+    "revision": "385fb1c32892c1bd9253",
+    "url": "/Thangzaliana-Thangtea/crud-react-redux/static/css/main.46cea12f.chunk.css"
+  },
+  {
+    "revision": "e619f176a531ffbad94a",
+    "url": "/Thangzaliana-Thangtea/crud-react-redux/static/css/1.56919101.chunk.css"
+  },
+  {
+    "revision": "b8e7d3a6416296e39778bcf72e7d1355",
+    "url": "/Thangzaliana-Thangtea/crud-react-redux/index.html"
+  }
+];
